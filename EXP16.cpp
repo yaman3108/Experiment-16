9b21169db8c3a63fcb6591af3764e6cc36ac7c7c
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-30 16:02:10 +0530

    Update EXP16.1.cpp

--- EXP16.cpp
+++ EXP16.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  float n1, n2, ans;
+  cout<<"Enter values of numbers 1 & 2: ";
+  cin>>n1>>n2;
+  try{
+    if(n2==0){
+      throw n2;
+    }
+    else{
+      ans=n1/n2;
+      cout<<"Answer = "<<ans<<endl;
+    }
+  }
+  catch(float num){
+    cout<<"\nERROR: Division by"<<num<<endl;
+  }
+}
